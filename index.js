289cc1d694e630b89a69c239d89335e423ce4c45
Vp <vilipulli.vp@gmail.com>
2020-12-03 17:49:39 +0200

    Add files via upload
    trash

---
+++ index.js
@@ -0,0 +1,138 @@
+const Discord = require('discord.js')
+const {Client, RichEmbed } = require('discord.js')
+const client = require('discord.js')
+const bot = new Client()
+ 
+const ping = require('minecraft-server-util')
+ 
+const token = 'NzQxOTM0NzM5MDk0NjM0NTc4.Xy-ykg.HKGr722v77HGH42BG6d1AvPsDVM'
+ 
+const PREFIX = '+'
+ 
+bot.on('ready', () =>{
+    console.log('Im online.')
+})
+ 
+bot.on('message', message =>{
+ 
+    let args = message.content.substring(PREFIX.length).split(' ')
+ 
+    switch(args[0]){
+        case 'mc':
+ 
+            if(!args[1]) return message.channel.send('You must type a minecraft server ip')
+            if(!args[2]) return message.channel.send('You must type a minecraft server port')
+ 
+            ping(args[1], parseInt(args[2]), (error, reponse) =>{
+                if(error) throw error
+                const Embed = new RichEmbed()
+                .setTitle('Server Status')
+                .addField('Server IP', reponse.host)
+                .addField('Server Version', reponse.version)
+                .addField('Online Players', reponse.onlinePlayers)
+                .addField('Max Players', reponse.maxPlayers)
+                
+                message.channel.send(Embed)
+            })
+        break
+ 
+    }
+ 
+})
+ 
+client.on('message', message => {
+  // Ignore messages that aren't from a guild
+  if (!message.guild) return;
+
+  // If the message content starts with "!kick"
+  if (message.content.startsWith('+kick')) {
+    // Assuming we mention someone in the message, this will return the user
+    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+    const user = message.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+      // Now we get the member from the user
+      const member = message.guild.member(user);
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Kick the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         */
+        member
+          .kick('Optional reason that will display in the audit logs')
+          .then(() => {
+            // We let the message author know we were able to kick the person
+            message.reply(`Successfully kicked ${user.tag}`);
+          })
+          .catch(err => {
+            // An error happened
+            // This is generally due to the bot not being able to kick the member,
+            // either due to missing permissions or role hierarchy
+            message.reply('I was unable to kick the member');
+            // Log the error
+            console.error(err);
+          });
+      } else {
+        // The mentioned user isn't in this guild
+        message.reply("That user isn't in this guild!");
+      }
+      // Otherwise, if no user was mentioned
+    } else {
+      message.reply("You didn't mention the user to kick!");
+    }
+  }
+});
+client.on('message', message => {
+  // Ignore messages that aren't from a guild
+  if (!message.guild) return;
+
+  // if the message content starts with "!ban"
+  if (message.content.startsWith('+ban')) {
+    // Assuming we mention someone in the message, this will return the user
+    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+    const user = message.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+      // Now we get the member from the user
+      const member = message.guild.member(user);
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Ban the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         * Read more about what ban options there are over at
+         * https://discord.js.org/#/docs/main/master/class/GuildMember?scrollTo=ban
+         */
+        member
+          .ban({
+            reason: 'They were bad!',
+          })
+          .then(() => {
+            // We let the message author know we were able to ban the person
+            message.reply(`Successfully banned ${user.tag}`);
+          })
+          .catch(err => {
+            // An error happened
+            // This is generally due to the bot not being able to ban the member,
+            // either due to missing permissions or role hierarchy
+            message.reply('I was unable to ban the member');
+            // Log the error
+            console.error(err);
+          });
+      } else {
+        // The mentioned user isn't in this guild
+        message.reply("That user isn't in this guild!");
+      }
+    } else {
+      // Otherwise, if no user was mentioned
+      message.reply("You didn't mention the user to ban!");
+    }
+  }
+});
+
+bot.login(token)
+
+
